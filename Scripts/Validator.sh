d1822260791d1872383122a3750a17e55d09ec76
Saikiran M <Saikiran.M@outlook.in>
2022-10-20 21:20:34 +0530

    Delete Scripts directory

--- Scripts/Validator.sh
+++
@@ -1,8 +0,0 @@
-##### Author : Saikiran M #####
-
-#!/bin/bash
-
-while read -p "Domain :: " site; do
-    echo -n "certificate expires on : "
-    echo | openssl s_client -servername $site -connect $site:443 2>/dev/null | openssl x509 -noout -enddate | cut -d '=' -f2
-done
